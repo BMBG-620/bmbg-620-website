b3c0b9935c1f710f0f5ace0992098d316e902238
BMBG-620 <connorsealey43@gmail.com>
2026-09-16 21:32:31 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://rkjopdllrihcluvtvhiy.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_VH_c7nSkvE9pFM76dlO-IA_dCXbFA-g";
+
+const APPLICATION_TABLE = "application";
+
+const IMAGE_BUCKET = "application-images";
